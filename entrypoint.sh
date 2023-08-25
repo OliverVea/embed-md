0944e10dbd52c2f4ccaeb07bb1a1db39316ec015
OliverVea <oliver.vea@gmail.com>
2023-08-26 02:45:06 +0900

    changed to verify

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,7 @@
 #!/usr/bin/env bash
 
-pwd
-ls
-
-# Configure git
-git config --global --add safe.directory /github/workspace
-git config user.name "embed-md bot"
-git remote set-url origin "$1"
-
-COMMIT_MESSAGE="$2"
-FILES="${@:3}"
+FILES="${@:1}"
 
 # Embed files
 echo "Embedding files: $FILES"
-npx embedme "$FILES"
-
-# Commit changes
-echo "Commiting with message: $COMMIT_MESSAGE"
-
-# Pull and apply changes
-git pull 
-
-# Stage and commit the changes
-git add "$FILES"
-git commit -m "$COMMIT_MESSAGE"
-
-# Push the changes to the current branch
-git push
+npx embedme "$FILES" --verify
